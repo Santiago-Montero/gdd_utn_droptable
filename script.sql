4feb55a9dbe3ca6f0730b92e39b107ca0ed0c992
emiliano fernandez <efernandezboleda@frba.utn.edu.ar>
2023-10-19 20:36:03 -0300

    Tablas Joaco Emi

--- script.sql
+++ script.sql
@@ -79,3 +79,120 @@ CREATE TABLE [DROP_TABLE].[Caracteristicas_por_inmueble](
      [id_caracteristicas] [int],
     CONSTRAINT fk_caracteristicas FOREIGN KEY ([id_caracteristicas]) REFERENCES [DROP_TABLE].[Caracteristicas]([id_caracteristicas])
 )
+
+
+CREATE TABLE [DROP_TABLE].[Barrio](
+	[id_barrio] [int] IDENTITY(1,1) PRIMARY KEY,
+	[nombre] VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE [DROP_TABLE].[Localidad](
+	[id_localidad] [int] IDENTITY(1,1) PRIMARY KEY,
+	[nombre] VARCHAR(100) NOT NULL,
+	[id_barrio] [int],
+	CONSTRAINT fk_localidad_barrio FOREIGN KEY ([id_barrio]) REFERENCES [DROP_TABLE].[Barrio]([id_barrio])
+);
+
+CREATE TABLE [DROP_TABLE].[Sucursal](
+	[id_sucursal] [int] IDENTITY(1,1) PRIMARY KEY,
+	[nombre] VARCHAR(100) NOT NULL,
+	[codigo] VARCHAR(100) NOT NULL,
+	[telefono] int,
+	[direccion] VARCHAR(100) NOT NULL,
+	[id_barrio] [int],
+	CONSTRAINT fk_sucursal_barrio FOREIGN KEY ([id_barrio]) REFERENCES [DROP_TABLE].[Barrio]([id_barrio])
+);
+
+
+CREATE TABLE [DROP_TABLE].[Alquiler](
+	[id_alquiler] [int] IDENTITY(1,1) NOT NULL,
+	[fecha_inicio] [date] ,
+	[fecha_fin] [date] ,
+	[deposito] [int] ,
+	[comision] [int] ,
+	[gastos] [int] ,
+	[codigo] [int] ,
+	[id_estado_alquiler] [int] ,
+	[id_anuncio] [int] ,
+	CONSTRAINT fk_anuncio_alquiler FOREIGN KEY ([id_anuncio]) REFERENCES [DROP_TABLE].Anuncio([id_anuncio])
+)
+
+CREATE TABLE [DROP_TABLE].[Moneda](
+	[id_moneda] [int] IDENTITY(1,1) NOT NULL,
+	[nombre] [varchar](100) NOT NULL,
+)
+
+CREATE TABLE [DROP_TABLE].[Tipo_Operacion](
+	[id_tipo_operacion] [int] IDENTITY(1,1) NOT NULL,
+	[nombre] [varchar](100) NOT NULL,
+
+
+)
+
+CREATE TABLE [DROP_TABLE].[Inquilino](
+	[id_inquilino] [int] IDENTITY(1,1) NOT NULL,
+	[id_persona] [int] ,
+	CONSTRAINT fk_persona_inquilino FOREIGN KEY ([id_persona]) REFERENCES [DROP_TABLE].[Persona]([id_persona]),
+	[id_alquiler] [int] ,
+	CONSTRAINT fk_alquiler_inquilino FOREIGN KEY ([id_alquiler]) REFERENCES [DROP_TABLE].[Alquiler]([id_alquiler])
+)
+
+
+CREATE TABLE [DROP_TABLE].[Persona](
+	[id_persona] [int] IDENTITY(1,1) NOT NULL,
+	[nombre] [varchar](100) NOT NULL,
+	[apellido] [varchar](100) NOT NULL,
+	[telefono] [int] ,
+	[mail] [varchar](25) NOT NULL,
+	[fecha_alta] [date] ,
+	[fecha_nacimiento] [date] ,
+)
+
+	CREATE TABLE [DROP_TABLE].[Agente](
+	[id_agente] [int] IDENTITY(1,1) NOT NULL,
+	[id_persona] [int] ,
+	CONSTRAINT fk_agente_persona FOREIGN KEY ([id_persona]) REFERENCES [DROP_TABLE].[Persona]([id_persona]),
+	[id_sucursal] [int] ,
+	CONSTRAINT fk_agente_sucursal FOREIGN KEY ([id_sucursal]) REFERENCES [DROP_TABLE].[Sucursal]([id_sucursal])
+	)
+
+	CREATE TABLE [DROP_TABLE].[Anuncio](
+	[id_anuncio] [int] IDENTITY(1,1) NOT NULL,
+	[fecha_Aplicacion] [date] ,
+	[tipo_operacion] [varchar](100) NOT NULL,
+	[precio_anuncio] [int] ,
+	[costoPublicacion] [int] ,
+	[fecha_Finalizacion] [date] ,
+	[id_agente] [int] ,
+	CONSTRAINT fk_anuncio_agente FOREIGN KEY ([id_agente]) REFERENCES [DROP_TABLE].[Agente]([id_agente]),
+	[id_inmueble] [int] ,
+	CONSTRAINT fk_anuncio_inmueble FOREIGN KEY ([id_inmueble]) REFERENCES [DROP_TABLE].Inmueble([id_inmueble]),
+	[id_moneda] [int] ,
+	CONSTRAINT fk_moneda_anuncio FOREIGN KEY ([id_moneda]) REFERENCES [DROP_TABLE].[Moneda]([id_moneda]),
+	[id_tipo_operacion] [int] ,
+	CONSTRAINT fk_tipo_operacion FOREIGN KEY ([id_tipo_operacion]) REFERENCES [DROP_TABLE].[Tipo_Operacion]([id_tipo_operacion]),
+	[id_estado_anuncio] [int] ,
+	CONSTRAINT fk_estado_anuncio FOREIGN KEY ([id_estado_anuncio]) REFERENCES [DROP_TABLE].[Estado_Anuncio]([id_estado_anuncio]),
+
+)
+
+
+	CREATE TABLE [DROP_TABLE].[Propietario](
+	[id_propietario] [int] IDENTITY(1,1) NOT NULL,
+	[id_persona] [int] NULL,
+	CONSTRAINT fk_propietario_persona FOREIGN KEY ([id_persona]) REFERENCES [DROP_TABLE].[Persona]([id_persona]),
+	[id_inmueble] [int] NULL,
+	CONSTRAINT fk_propietario_inmueble FOREIGN KEY ([id_inmueble]) REFERENCES [DROP_TABLE].[Inmueble]([id_inmueble])
+	)
+
+	CREATE TABLE [DROP_TABLE].[Venta](
+	[id_venta] [int] IDENTITY(1,1) NOT NULL,
+	[fecha_venta] [date] ,
+	[precio_venta] [int] ,
+	[comision_inmobiliaria] [int] ,
+	[codigo] [int] ,
+	[id_moneda] [int],
+	CONSTRAINT fk_moneda_venta FOREIGN KEY ([id_moneda]) REFERENCES [DROP_TABLE].Moneda([id_moneda])
+	[id_anuncio] [int] ,
+	CONSTRAINT fk_anuncio_alquiler FOREIGN KEY ([id_anuncio]) REFERENCES [DROP_TABLE].Anuncio([id_anuncio])
+)
